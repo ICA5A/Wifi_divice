046a152214ee0d92ba55502ad51a52a792253aee
ICASA <70022933+ICA5A@users.noreply.github.com>
2020-08-26 13:49:26 -0500

    Adicion para interfaz como Sonoff pantalla 1

--- app/src/main/java/com/example/icasa_2/AddDevice1.java
+++ app/src/main/java/com/example/icasa_2/AddDevice1.java
@@ -2,13 +2,41 @@ package com.example.icasa_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddDevice1 extends AppCompatActivity {
+
+    Button next, back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_device1);
+
+        next = findViewById(R.id.btnNext);
+        back = findViewById(R.id.btnBack);
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddDevice2.class);
+               // intent.putExtra("user", id);
+                startActivity(intent);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+               // intent.putExtra("user", id);
+                startActivity(intent);
+            }
+        });
+
     }
 }
